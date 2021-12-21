021e10fb602bf95aca52578bd985f82536a60313
砼砼 <code@tossp.com>
2021-12-21 23:50:49 +0800

    AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -19,34 +19,54 @@ Firmware_Diy_Core() {
 	INCLUDE_Original_OpenWrt_Compatible=true
 }
 
-Firmware-Diy() {
+Firmware_Diy() {
 
-	# 部分可调用变量如下
-	# OP_Maintainer		源码作者
-	# OP_REPO_NAME		仓库名称
-	# OP_BRANCH		源码分支
-	# TARGET_PROFILE	设备名称
-	# TARGET_BOARD		设备架构
+	# 请在该函数内定制固件
 
-	# CustomFiles		仓库 /CustomFiles 绝对路径
-	# Scripts		仓库 /Scripts 绝对路径
-	# Home			源码存放绝对路径,等同 ${GITHUB_WORKSPACE}/openwrt
-	# feeds_luci		绝对路径,等同 ${GITHUB_WORKSPACE}/openwrt/package/feeds/luci
-	# feeds_pkgs		绝对路径,等同 ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages
-	# base_files		绝对路径,等同 ${GITHUB_WORKSPACE}/openwrt/package/base-files/files
+	# 可用预设变量, 其他可用变量请参考运行日志
+	# ${OP_AUTHOR}			OpenWrt 源码作者
+	# ${OP_REPO}			OpenWrt 仓库名称
+	# ${OP_BRANCH}			OpenWrt 源码分支
+	# ${TARGET_PROFILE}		设备名称
+	# ${TARGET_BOARD}		设备架构
+	# ${TARGET_FLAG}		固件名称后缀
 
-	case "${OP_Maintainer}/${OP_REPO_NAME}:${OP_BRANCH}" in
+	# ${Home}				OpenWrt 源码位置
+	# ${CONFIG_FILE}		使用的配置文件名称
+	# ${FEEDS_CONF}			OpenWrt 源码目录下的 feeds.conf.default 文件
+	# ${CustomFiles}		仓库中的 /CustomFiles 绝对路径
+	# ${Scripts}			仓库中的 /Scripts 绝对路径
+	# ${FEEDS_LUCI}			OpenWrt 源码目录下的 package/feeds/luci 目录
+	# ${FEEDS_PKG}			OpenWrt 源码目录下的 package/feeds/packages 目录
+	# ${BASE_FILES}			OpenWrt 源码目录下的 package/base-files/files 目录
+
+	case "${OP_AUTHOR}/${OP_REPO}:${OP_BRANCH}" in
 	coolsnowwolf/lede:master)
-		sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${feeds_pkgs}/ttyd/files/ttyd.config
+		sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${FEEDS_PKG}/ttyd/files/ttyd.config
+		AddPackage git lean luci-theme-argon jerrykuku 18.06
+		AddPackage git lean luci-app-argon-config jerrykuku master
+		AddPackage git other AutoBuild-Packages Hyy2001X master
+		AddPackage svn other luci-app-smartdns kenzok8/openwrt-packages/trunk
+		AddPackage svn other luci-app-socat Lienol/openwrt-package/trunk
+		AddPackage svn other luci-app-eqos kenzok8/openwrt-packages/trunk
+		AddPackage git other OpenClash vernesong master
+		AddPackage git other luci-app-usb3disable rufengsuixing master
+		# AddPackage git other OpenAppFilter destan19 master
+		# AddPackage svn other luci-app-ddnsto linkease/nas-packages/trunk/luci
+		# AddPackage svn other ddnsto linkease/nas-packages/trunk/network/services
+
+		case "${TARGET_PROFILE}" in
+		d-team_newifi-d2)
+			patch -i ${CustomFiles}/${TARGET_PROFILE}_mac80211.patch package/kernel/mac80211/files/lib/wifi/mac80211.sh
+			Copy ${CustomFiles}/${TARGET_PROFILE}_system ${BASE_FILES}/etc/config system
+			sed -i "/DEVICE_COMPAT_VERSION := 1.1/d" target/linux/ramips/image/mt7621.mk
+		;;
+		x86_64)
+			AddPackage git other openwrt-passwall xiaorouji main
+			rm -rf packages/lean/autocore
+			AddPackage git lean autocore-modify Hyy2001X master
+		;;
+		esac
 	;;
 	esac
-
-	case "${TARGET_PROFILE}" in
-	d-team_newifi-d2)
-		patch -i ${CustomFiles}/mac80211_d-team_newifi-d2.patch package/kernel/mac80211/files/lib/wifi/mac80211.sh
-		Copy ${CustomFiles}/system_d-team_newifi-d2 ${base_files}/etc/config system
-		[[ ${OP_REPO_NAME} == lede ]] && sed -i "/DEVICE_COMPAT_VERSION := 1.1/d" target/linux/ramips/image/mt7621.mk
-	;;
-	esac
-
 }
