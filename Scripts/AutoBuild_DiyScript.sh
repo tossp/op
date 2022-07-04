f3f31da1f6e02145cf37f98f708ea97ea22435a3
砼砼 <code@tossp.com>
2022-07-05 01:42:58 +0800

    Create pve

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -9,14 +9,12 @@ Firmware_Diy_Core() {
 	Default_IP="192.168.9.1"
 	Banner_Message="Powered by TossP.com"
 
-	Short_Firmware_Date=true
-	Checkout_Virtual_Images=true
-	Firmware_Format=AUTO
-	REGEX_Skip_Checkout="packages|buildinfo|sha256sums|manifest|kernel|rootfs|factory"
+	Short_Fw_Date=true
+	x86_Full_Images=true
+	Fw_Format=false
+	Regex_Skip="packages|buildinfo|sha256sums|manifest|kernel|rootfs|factory|itb|profile"
 
-	INCLUDE_AutoBuild_Features=true
-	INCLUDE_DRM_I915=false
-	INCLUDE_Original_OpenWrt_Compatible=true
+	AutoBuild_Features=true
 }
 
 Firmware_Diy() {
@@ -31,7 +29,7 @@ Firmware_Diy() {
 	# ${TARGET_BOARD}		设备架构
 	# ${TARGET_FLAG}		固件名称后缀
 
-	# ${Home}				OpenWrt 源码位置
+	# ${WORK}				OpenWrt 源码位置
 	# ${CONFIG_FILE}		使用的配置文件名称
 	# ${FEEDS_CONF}			OpenWrt 源码目录下的 feeds.conf.default 文件
 	# ${CustomFiles}		仓库中的 /CustomFiles 绝对路径
@@ -42,31 +40,47 @@ Firmware_Diy() {
 
 	case "${OP_AUTHOR}/${OP_REPO}:${OP_BRANCH}" in
 	coolsnowwolf/lede:master)
+		rm -r $(PKG_Finder d "package feeds" luci-theme-argon)
 		sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${FEEDS_PKG}/ttyd/files/ttyd.config
+
 		AddPackage git lean luci-theme-argon jerrykuku 18.06
 		AddPackage git lean luci-app-argon-config jerrykuku master
-		AddPackage git other AutoBuild-Packages Hyy2001X master
-		AddPackage svn other luci-app-smartdns kenzok8/openwrt-packages/trunk
-		AddPackage svn other luci-app-socat Lienol/openwrt-package/trunk
-		AddPackage svn other luci-app-eqos kenzok8/openwrt-packages/trunk
+		AddPackage svn other luci-app-smartdns immortalwrt/luci/branches/openwrt-18.06/applications
+		sed -i 's/..\/..\//\$\(TOPDIR\)\/feeds\/luci\//g' $(PKG_Finder d package luci-app-smartdns)/Makefile
+		AddPackage svn other luci-app-eqos immortalwrt/luci/branches/openwrt-18.06/applications
+		sed -i 's/..\/..\//\$\(TOPDIR\)\/feeds\/luci\//g' $(PKG_Finder d package luci-app-eqos)/Makefile
+		AddPackage svn other luci-app-socat immortalwrt/luci/branches/openwrt-18.06/applications
+		sed -i 's/..\/..\//\$\(TOPDIR\)\/feeds\/luci\//g' $(PKG_Finder d package luci-app-socat)/Makefile
 		AddPackage git other OpenClash vernesong master
-		AddPackage git other luci-app-usb3disable rufengsuixing master
-		# AddPackage git other OpenAppFilter destan19 master
-		# AddPackage svn other luci-app-ddnsto linkease/nas-packages/trunk/luci
-		# AddPackage svn other ddnsto linkease/nas-packages/trunk/network/services
+		AddPackage git other luci-app-ikoolproxy iwrt main
+		AddPackage git other helloworld fw876 master
+		sed -i 's/143/143,8080,8443/' $(PKG_Finder d package luci-app-ssr-plus)/root/etc/init.d/shadowsocksr
+
+		patch < ${CustomFiles}/Patches/fix_shadowsocksr_alterId.patch -p1 -d ${WORK}
+		patch < ${CustomFiles}/Patches/fix_ntfs3_conflict_with_antfs.patch -p1 -d ${WORK}
+		patch < ${CustomFiles}/Patches/fix_aria2_auto_create_download_path.patch -p1 -d ${WORK}
 
 		case "${TARGET_PROFILE}" in
 		d-team_newifi-d2)
-			patch -i ${CustomFiles}/${TARGET_PROFILE}_mac80211.patch package/kernel/mac80211/files/lib/wifi/mac80211.sh
 			Copy ${CustomFiles}/${TARGET_PROFILE}_system ${BASE_FILES}/etc/config system
 			sed -i "/DEVICE_COMPAT_VERSION := 1.1/d" target/linux/ramips/image/mt7621.mk
+			Copy ${CustomFiles}/Depends/automount $(PKG_Finder d "package" automount)/files 15-automount
+			patch < ${CustomFiles}/d-team_newifi-d2_mt76_dualband.patch -p1 -d ${WORK}
+		;;
+		xiaoyu_xy-c5)
+			Copy ${CustomFiles}/Depends/automount $(PKG_Finder d "package" automount)/files 15-automount
 		;;
 		x86_64)
-			AddPackage git other openwrt-passwall xiaorouji main
+			AddPackage git passwall-depends openwrt-passwall xiaorouji packages
+			AddPackage git passwall-luci openwrt-passwall xiaorouji luci
 			rm -rf packages/lean/autocore
 			AddPackage git lean autocore-modify Hyy2001X master
+			cat ${CustomFiles}/x86_64_Kconfig >> ${WORK}/target/linux/x86/config-5.15
 		;;
 		esac
+	;;
+	immortalwrt/immortalwrt*)
+		sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${FEEDS_PKG}/ttyd/files/ttyd.config
 	;;
 	esac
 }
